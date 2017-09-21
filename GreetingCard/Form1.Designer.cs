1b737e18090a5da21e437c5e0f65851b96e55839
GarrSaun <garrsaun253@ed.amdsb.ca>
2017-09-21 12:24:42 -0400

    Add project files.

---
+++ GreetingCard/Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace GreetingCard
+{
+    partial class serbiaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // serbiaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::GreetingCard.Properties.Resources.Stronk;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Name = "serbiaForm";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.serbiaForm_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ GreetingCard/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace GreetingCard
+{
+    public partial class serbiaForm : Form
+    {
+        public serbiaForm()
+        {
+            InitializeComponent();
+
+
+        
+        }
+
+        private void serbiaForm_Click(object sender, EventArgs e)
+        {
+            //set up graphics and sound  
+            BackgroundImage = null;
+            Graphics onScreen = this.CreateGraphics();
+            Bitmap bm = new Bitmap(this.Width, this.Height);
+            Graphics offScreen = Graphics.FromImage(bm);
+            Pen drawPen = new Pen(Color.Black, 5);
+            Pen skinPen = new Pen(Color.NavajoWhite, 8);
+            SolidBrush slavBrush = new SolidBrush(Color.Brown);
+            SolidBrush skinBrush = new SolidBrush(Color.NavajoWhite);
+            SolidBrush accBrush = new SolidBrush(Color.Gray);
+
+            SoundPlayer slavPlayer = new SoundPlayer(Properties.Resources.Remove);
+
+            //Image
+            slavPlayer.Play();
+
+            onScreen.DrawLine(skinPen, 200, 125, 125, 250); //left arm
+            onScreen.DrawLine(skinPen, 300, 125, 350, 250); //right arm
+            onScreen.DrawRectangle(drawPen, 200, 125, 100, 125); //torso
+            onScreen.FillRectangle(slavBrush, 200, 125, 100, 125); //torso fill
+            onScreen.DrawEllipse(drawPen, 213, 70, 75, 75); //head
+            onScreen.FillEllipse(skinBrush, 213, 70, 75, 75); //head fill
+        }
+    }
+}
